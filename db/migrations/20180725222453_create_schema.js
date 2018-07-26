5e82769ec5871f697ea115fddba8b93d8a68d8ce
Michael Surya <tomichaels@gmail.com>
2018-07-26 05:01:12 +0000

    created data base schema with no seed

---
+++ db/migrations/20180725222453_create_schema.js
@@ -0,0 +1,55 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('Events', function (table) { 
+        table.increments();
+        table.string('title');
+        table.string('description');
+        table.string('URL');
+        table.string('location');
+        }).then(data => {        
+            return knex.schema.createTable('Attendees', function (table) {
+            table.increments();
+            table.string('name');
+            table.string('email');
+            table.integer('event_id').unsigned();
+            table.foreign('event_id').references('Events.id');
+        }).then(data => {
+            return knex.schema.createTable('Times', function (table) {
+            table.increments();
+            table.string('start_time');
+            table.string('end_time');
+            table.date('date');
+            }).then(data => {
+                return knex.schema.createTable('Attendees_Times', function (table) {
+                table.integer('attendee_id').unsigned();
+                table.foreign('attendee_id').references('Attendees.id');
+                table.integer('time_id').unsigned();
+                table.foreign('time_id').references('Times.id');
+                }).then(data => {
+                    return knex.schema.createTable('Events_Times', function (table) {
+                    table.integer('event_id').unsigned();
+                    table.foreign('event_id').references('Events.id');
+                    table.integer('time_id').unsigned();
+                    table.foreign('time_id').references('Times.id');
+                    })
+                })
+            })
+        })
+    })
+}
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('Events_Times', function (table) { 
+        }).then(data => {        
+            return knex.schema.dropTable('Attendees_Times', function (table) {
+        }).then(data => {
+            return knex.schema.dropTable('Times', function (table) {
+            }).then(data => {
+                return knex.schema.dropTable('Attendees', function (table) {
+                }).then(data => {
+                    return knex.schema.dropTable('Events', function (table) {
+                    })
+                })
+            })
+        })
+    })
+}
