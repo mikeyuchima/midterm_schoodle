6459e4b72fcc2923d6cf6fbd402ab8609ec74f69
Michael Surya <tomichaels@gmail.com>
2018-07-26 18:56:13 +0000

    seed data completed

--- db/migrations/20180725222453_create_schema.js
+++ db/migrations/20180725222453_create_schema.js
@@ -1,31 +1,31 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('Events', function (table) { 
+    return knex.schema.createTable('events', function (table) { 
         table.increments('id').primary();
         table.string('title');
         table.string('description');
         table.string('URL');
         table.string('location');
         }).then(() => {        
-            return knex.schema.createTable('Attendees', function (table) {
+            return knex.schema.createTable('attendees', function (table) {
             table.increments('id').primary()
             table.string('name');
             table.string('email');
             table.integer('event_id').unsigned();
-            table.foreign('event_id').references('Events.id');
+            table.foreign('event_id').references('events.id');
         }).then(() => {
-            return knex.schema.createTable('Times', function (table) {
+            return knex.schema.createTable('times', function (table) {
             table.increments('id').primary()
             table.string('start_time');
             table.string('end_time');
             table.date('date');
             table.integer('event_id').unsigned();
-            table.foreign('event_id').references('Events.id');
+            table.foreign('event_id').references('events.id');
             }).then(() => {
-                return knex.schema.createTable('Attendees_Times', function (table) {
+                return knex.schema.createTable('attendees_times', function (table) {
                 table.integer('attendee_id').unsigned();
-                table.foreign('attendee_id').references('Attendees.id');
+                table.foreign('attendee_id').references('attendees.id');
                 table.integer('time_id').unsigned();
-                table.foreign('time_id').references('Times.id');
+                table.foreign('time_id').references('times.id');
                 })
             })
         })
@@ -33,13 +33,13 @@ exports.up = function(knex, Promise) {
 }
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('Attendees_Times', function (table) {
-    }).then(data => {
-        return knex.schema.dropTable('Times', function (table) {
-        }).then(data => {
-            return knex.schema.dropTable('Attendees', function (table) {
-            }).then(data => {
-                return knex.schema.dropTable('Events', function (table) {
+    return knex.schema.dropTable('attendees_times', function () {
+    }).then(() => {
+        return knex.schema.dropTable('times', function () {
+        }).then(() => {
+            return knex.schema.dropTable('attendees', function () {
+            }).then(() => {
+                return knex.schema.dropTable('events', function () {
                 })
             })
         })
--- db/seeds/seed_schema.js
+++ db/seeds/seed_schema.js
@@ -1,15 +1,14 @@
 exports.seed = function(knex, Promise) {
-    return knex('Events').del()
+    let id = 0;
+    return knex('events').del()
       .then(function () {
-          return knex('Events').insert({title: 'Lighthouse Labs', description: 'JS Fundamentals', URL: 'https://www.lighthouselabs.com', location: '46 Spadina, Toronto'}).returning("*")
+          return knex('events').insert({title: 'Lighthouse Labs', description: 'JS Fundamentals', URL: 'https://www.lighthouselabs.com', location: '46 Spadina, Toronto'}).returning("id");
       }).then(event => {
-          console.log(event[0].id)
-          let id = event[0].id
-          knex('Times').insert({start_time: '7:30 PM', end_time: '9:00 PM', date: 'July 30, 2018', event_id: 'id'}).returning("*")
-      }).then(event => {
-          console.log(event)
-          return knex('Attendees').insert({name: 'Dexter', email: 'boyGenius@TheGreat.com', event_id: event[0]}).returning('event_id')
-      }).then(event => {
-        return knex('Attendees_Times').insert({attendee_id: event[0], event_id: event[0]})
+          id = event[0];
+          return knex('times').insert({start_time: '7:30 PM', end_time: '9:00 PM', date: 'July 30, 2018', event_id: id}).returning("event_id");
+      }).then(() => {
+          return knex('attendees').insert({name: 'Dexter', email: 'boyGenius@TheGreat.com', event_id: id}).returning('event_id');
+      }).then(() => {
+        return knex('attendees_times').insert({attendee_id: id, time_id: id});
   });
 }
