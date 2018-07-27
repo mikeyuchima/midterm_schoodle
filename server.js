e7168e92da81f3b1fecb003286ef4f7f6d8a4ac9
elaradi23 <ta.elaradi23@gmail.com>
2018-07-27 20:25:20 +0000

    Fixed async problem with get events/hash

--- server.js
+++ server.js
@@ -37,48 +37,110 @@ app.use(express.static("public"));
 
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
-// app.use("/events/", eventsRoutes);
 
 // Home page
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/event/:hash", (req, res) => {
+// app.get("/event/:hash", (req, res) => {
+
+//   let templateVars = {};
+
+//   knex
+//   .select()
+//   .from("events")
+//   .limit(1)
+//   .where("hash", '=', req.params.hash)
+//   .then((event) => {
+//     templateVars = event[0];
+
+//     Promise.all([
+//       knex
+//       .select()
+//       .from("times")
+//       .where('times.event_id', '=', event[0].id)
+//       .then((times) => {
+//         templateVars.timeSlots = [];
+//         for(var time in times){ templateVars.timeSlots.push(times[time]); }
+//       }),
+//       knex
+//       .select()
+//       .from("attendees")
+//       .where('attendees.event_id', '=', event[0].id)
+//       .then((attendees) => {
+//         templateVars.attendees = [];
+//         for(var person in attendees){
+
+//           templateVars.attendees.push(attendees[person]);
+
+//           knex
+//             .select()
+//             .from('attendees_times')
+//             .innerJoin('times', 'attendees_times.time_id', 'times.id')
+//             .where('attendees_times.attendee_id','=', attendees[person].id)
+//             .then((availabilities) => {
+//               templateVars.attendees[person].times_available = [];
+
+//               for(var availability in availabilities){
+//                 console.log('>>', availabilities[availability]);
+//                 templateVars.attendees[person].times_available.push(availabilities[availability]);
+//               }
+//           });
+//         }
+//       })
+//     ])
+//     .then(() => {
+//       console.log(templateVars);
+//       res.render('event', templateVars);
+//     });
+//   });
+// });
+
+app.get("/event/:hash", async (req, res) => {
 
   let templateVars = {};
 
-  knex
-  .select()
-  .from("events")
-  .limit(1)
-  .where("hash", '=', req.params.hash)
-  .then((event) => {
-    templateVars = event[0];
+  templateVars.event = await knex
+    .select()
+    .from("events")
+    .limit(1)
+    .where("hash", '=', req.params.hash)
 
-    Promise.all([
-      knex
-      .select()
-      .from("times")
-      .where('times.event_id', '=', event[0].id)
-      .then((times) => {
-        templateVars.timeSlots = [];
-        for(var time in times){ templateVars.timeSlots.push(times[time]); }
-      }),
-      knex
+  templateVars.times = await knex
+    .select()
+    .from("times")
+    .where('times.event_id', '=', templateVars.event[0].id)
+
+
+  templateVars.attendees = await knex
       .select()
       .from("attendees")
-      .where('attendees.event_id', '=', event[0].id)
-      .then((attendees) => {
-        templateVars.attendees = [];
-        for(var person in attendees){ templateVars.attendees.push(attendees[person]); }
-      })
-    ])
-    .then(() => {
-      console.log(templateVars);
-      res.render('event', templateVars);
-    });
-  });
+      .where('attendees.event_id', '=', templateVars.event[0].id)
+
+  try {
+    for(var person in templateVars.attendees){
+      await knex
+        .select()
+        .from('attendees_times')
+        .innerJoin('times', 'attendees_times.time_id', 'times.id')
+        .where('attendees_times.attendee_id','=', templateVars.attendees[person].id)
+        .then((availabilities) => {
+          templateVars.attendees[person].times_available = [];
+
+          for(var availability in availabilities){
+            console.log('>>', availabilities[availability]);
+            templateVars.attendees[person].times_available.push(availabilities[availability]);
+          }
+      });
+    }
+  } catch(e) {
+    console.log(e)
+  }
+
+  console.log(templateVars.attendees[0].times_available);
+
+  res.render('event', templateVars);
 });
 
 // Create Event
